45fc8269bf61ef4f0018cbbe8563f2d000081234
Davide Galassi <davxy@datawok.net>
2026-03-09 11:10:22 +0100

    Update bench results

--- benches/common.rs
+++ benches/common.rs
@@ -17,10 +17,34 @@ fn bench_vrf_output<S: BenchInfo>(c: &mut Criterion) {
     });
 }
 
-fn bench_data_to_point<S: BenchInfo>(c: &mut Criterion) {
-    let name = format!("{}/data_to_point", S::SUITE_NAME);
+fn bench_data_to_point_tai<S: BenchInfo>(c: &mut Criterion) {
+    let name = format!("{}/data_to_point_tai", S::SUITE_NAME);
     c.bench_function(&name, |b| {
-        b.iter(|| S::data_to_point(black_box(b"bench input data")).unwrap());
+        b.iter(|| {
+            ark_vrf::utils::hash_to_curve_tai::<S>(black_box(b"bench input data")).unwrap()
+        });
+    });
+}
+
+fn bench_data_to_point_ell2<S: BenchInfo>(c: &mut Criterion)
+where
+    ark_vrf::CurveConfig<S>: ark_ec::twisted_edwards::TECurveConfig,
+    ark_vrf::CurveConfig<S>: ark_ec::hashing::curve_maps::elligator2::Elligator2Config,
+    ark_ec::hashing::curve_maps::elligator2::Elligator2Map<ark_vrf::CurveConfig<S>>:
+        ark_ec::hashing::map_to_curve_hasher::MapToCurve<
+            <ark_vrf::AffinePoint<S> as ark_ec::AffineRepr>::Group,
+        >,
+{
+    let h2c_suite_id = b"Bandersnatch_XMD:SHA-512_ELL2_RO_";
+    let name = format!("{}/data_to_point_ell2", S::SUITE_NAME);
+    c.bench_function(&name, |b| {
+        b.iter(|| {
+            ark_vrf::utils::hash_to_curve_ell2::<S, sha2::Sha512>(
+                black_box(b"bench input data"),
+                h2c_suite_id,
+            )
+            .unwrap()
+        });
     });
 }
 
@@ -96,7 +120,7 @@ fn bench_delinearize<S: BenchInfo>(c: &mut Criterion) {
 fn bench_common_suite<S: BenchInfo>(c: &mut Criterion) {
     S::print_info();
     bench_vrf_output::<S>(c);
-    bench_data_to_point::<S>(c);
+    bench_data_to_point_tai::<S>(c);
     bench_point_to_hash::<S>(c);
     bench_challenge::<S>(c);
     bench_nonce::<S>(c);
@@ -106,6 +130,10 @@ fn bench_common_suite<S: BenchInfo>(c: &mut Criterion) {
 fn bench_common(c: &mut Criterion) {
     // Per-suite benchmarks.
     for_each_suite!(c, bench_common_suite);
+
+    // ELL2 bench only for suites that support it.
+    #[cfg(feature = "bandersnatch")]
+    bench_data_to_point_ell2::<ark_vrf::suites::bandersnatch::BandersnatchSha512Ell2>(c);
 }
 
 criterion_group!(benches, bench_common);
